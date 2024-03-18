15b760e82d0cfa78b6c074adb473432f72629072
cocktailpeanut <cocktailpeanuts@proton.me>
2024-03-18 17:38:44 -0400

    init

---
+++ conda/pinokio.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: "test",
+  description: "test",
+  menu: [{
+    text: "index.json",
+    href: "index.json"
+  }]
+}
---
+++ pinokio.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: "Test",
+  icon: "icon.jpg",
+  menu: [{
+    text: "script call",
+    href: "script/index.json"
+  }, {
+    text: "args",
+    href: "script/args.json"
+  }]
+}
