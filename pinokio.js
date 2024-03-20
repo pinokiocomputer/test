17e264e844b98591d8c419dbf36ca54b8289b103
cocktailpeanut <cocktailpeanuts@proton.me>
2024-03-19 21:13:33 -0400

    update

--- pinokio.js
+++ pinokio.js
@@ -9,10 +9,19 @@ module.exports = {
     href: "args/index.json"
   }, {
     text: "torch install",
-    href: "torch/install.json"
+    href: "script_run/install.json"
   }, {
     text: "torch shared install",
-    href: "torch/shared_venv_install.json"
+    href: "script_run/shared_venv_install.json"
+  }, {
+    text: "branch install",
+    href: "script_run/branch_install.json"
+  }, {
+    text: "commit install",
+    href: "script_run/commit_install.json"
+  }, {
+    text: "latest install",
+    href: "script_run/latest_install.json"
   }, {
     text: "update",
     href: "update.json"
