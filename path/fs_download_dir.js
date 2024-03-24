84980329d289d92737a962c954b52dabf7f67b02
cocktailpeanut <cocktailpeanuts@proton.me>
2024-03-23 21:52:04 -0400

    update

--- path/fs_download_dir.js
+++ path/fs_download_dir.js
@@ -3,7 +3,7 @@ module.exports = {
     "method": "fs.download",
     "params": {
       "uri": "https://raw.githubusercontent.com/pinokiocomputer/test/main/update.json",
-      "path": "."
+      "path": "{{cwd}}"
     }
   }]
 }
