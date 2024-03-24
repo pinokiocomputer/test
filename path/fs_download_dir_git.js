7f488a0340dc287b8846bb56b8f198a1bd7ae46e
cocktailpeanut <cocktailpeanuts@proton.me>
2024-03-23 21:48:37 -0400

    update

---
+++ path/fs_download_dir_git.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "run": [{
+    "method": "fs.download",
+    "params": {
+      "uri": "https://raw.githubusercontent.com/pinokiocomputer/test/main/update.json",
+      "path": "https://github.com/pinokiocomputer/test.git/path"
+    }
+  }]
+}
---
+++ path/fs_download_git.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "run": [{
+    "method": "fs.download",
+    "params": {
+      "uri": "https://jsonplaceholder.typicode.com/todos",
+      "path": "https://github.com/pinokiocomputer/test.git/path/todos2.json",
+    }
+  }]
+}
