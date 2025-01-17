874c9bdf1c5a7fd01c95e12736716c79b2c1edd6
cocktailpeanut <cocktailpeanuts@proton.me>
2025-01-17 15:57:51 -0500

    update

--- which/index.js
+++ which/index.js
@@ -3,19 +3,25 @@ module.exports = {
     when: "{{which('brew')}}",
     method: "shell.run",
     params: {
-      "{{which('brew')}}"
+      message: "{{which('brew')}}"
+    },
+  }, {
+    when: "{{which('xcode-select')}}",
+    method: "shell.run",
+    params: {
+      message: "{{which('xcode-select')}}"
     },
   }, {
     when: "{{which('winget')}}",
     method: "shell.run",
     params: {
-      "{{which('winget')}}"
+      message: "{{which('winget')}}"
     },
   }, {
     when: "{{which('apt-get')}}",
     method: "shell.run",
     params: {
-      "{{which('apt-get')}}"
+      message: "{{which('apt-get')}}"
     },
   }]
 }
